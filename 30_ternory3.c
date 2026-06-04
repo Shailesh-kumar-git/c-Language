47843e93c914255859f6f6275aba90a067cbdb2e
Shailesh kumar git <shaileshkumar888889@gmail.com>
2026-06-04 16:12:45 +0530

    new program

---
+++ 30_ternory3.c
@@ -0,0 +1,27 @@
+// wright a prg to check voter is elegebal to give vote or not
+
+/* #include<stdio.h>
+
+int main(){
+    int age;
+    printf("enter your age = ");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("persion can give vote");
+    }
+    else{
+        printf("persion can't give vote");
+    }
+    return 0;
+}*/
+
+#include<stdio.h>
+
+int main(){
+    int age;
+    printf("enter your age = ");
+    scanf("%d",&age);
+    age>=20 ? printf("you can give vote") : printf("you can't give vote");
+    return 0;
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# c-Language
-program
