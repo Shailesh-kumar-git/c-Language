4e585b57ebb1ad9f16563bb9b9c46ae497b9ac24
Shailesh kumar git <shaileshkumar888889@gmail.com>
2026-06-08 20:12:56 +0530

    add

--- 64_step-3.c
+++ 64_step-3.c
@@ -7,3 +7,25 @@
 // An additional surcharge of 20% is added to the bill
 
 
+#include<stdio.h>
+
+int main(){
+    int unit;
+    float r;
+    printf("enter electricity unit :- ");
+    scanf("%d",&unit);
+
+    if(unit <= 50){
+        r = unit * 0.50;
+    } else if(unit <= 150){
+        r = 50 * 0.50 + (unit - 50) * 0.75;
+    } else if(unit <= 250){
+        r = 50 * 0.50 + 100 * 0.75 + (unit - 150) * 1.20;
+    } else {
+        r = 50 * 0.50 + 100 * 0.75 + 100 * 1.20 + (unit - 250) * 1.50;
+    }
+
+    r = r * 1.20; /* add 20% surcharge */
+    printf("total amount for given unit = %.2f\n", r);
+    return 0;
+}
