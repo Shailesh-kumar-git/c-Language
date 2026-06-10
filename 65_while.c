0c5df7e1eca179bada40d568ef92bbe9508ac633
shailesh <shaileshkumar888889@gmail.com>
2026-06-10 01:39:21 -0700

    add

---
+++ 65_while.c
@@ -0,0 +1,9 @@
+// example of switch case
+
+#include <stdio.h>
+
+int main() {
+    int a=23;
+    printf("entered %d",a);
+    return 0;
+}
---
+++ 66_switch.2.c
@@ -0,0 +1,17 @@
+// 7. Write a program using switch-case to print your friend's 
+// name based on the first letter of their name.
+
+#include <stdio.h>
+int main() {
+    char c;
+    printf("enter firat letter of your frends name : ");
+    scanf("%c",&c);
+    switch(c){
+        case 's': printf("siddant"); break;
+        case 'a': printf("anshu"); break;
+        case 'm': printf("mom"); break;
+        case 'u': printf("umang"); break;
+
+    }
+    return 0;
+}
